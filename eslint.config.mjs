0043f74d1fb375d9ad62f39456b78004482bdcf5
ysknsid25 <kengo071225@gmail.com>
2026-02-09 18:33:35 +0900

    initial commit
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,33 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    stylistic.configs.customize({
+        indent: 2,
+        quotes: "single",
+        semi: true,
+        jsx: false,
+    }),
+    {
+        ignores: ["dist", "coverage", ".git", "node_modules", "test"],
+    },
+    ...storybook.configs["flat/recommended"],
 ]);
 
 export default eslintConfig;
